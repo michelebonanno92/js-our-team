d1841d5b2a4aaeb2176a0d1442a4de6a3857c588
micheleboanno92 <michelebonanno92@gmail.com>
2024-07-26 09:19:54 +0200

    creato l'array del team con informazioni

--- js/script.js
+++ js/script.js
@@ -26,3 +26,69 @@ Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
 Scott Estrada	Developer	            scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
  */
+
+// array del team 
+
+
+const team = [
+    {
+
+      name: ' Wayne Barnett',
+      role: '	Founder & CEO	',
+      image:  ' wayne-barnett-founder-ceo.jpg	 ',
+        
+    },
+
+    {
+
+      name: ' Angela Caroll',
+      role: '	Chief Editor	',
+      image:  '  angela-caroll-chief-editor.jpg	 ',
+        
+    },
+
+    {
+
+      name: ' Walter Gordon',
+      role: '	Office Manager	',
+      image:  ' walter-gordon-office-manager.jpg ',
+        
+    },
+
+    {
+
+      name: ' Angela Lopez',
+      role: ' Developer ',
+      image:  '  scott-estrada-developer.jpg ',
+        
+    },
+
+    {
+
+      name: ' Scott Estrada',
+      role: '	Social Media Manager	',
+      image:  ' angela-lopez-social-media-manager.jpg ',
+        
+    },
+
+    {
+
+      name: ' Barbara Ramos ',
+      role: '	Graphic Designer ',
+      image:  '  barbara-ramos-graphic-designer.jpg ',
+        
+    },
+
+
+
+
+ ];
+console.log(team[0]) ;
+console.log(team[1]) ;
+console.log(team[2]) ;
+console.log(team[3]) ;
+console.log(team[4]) ;
+console.log(team[5]) ;
+
+
+
